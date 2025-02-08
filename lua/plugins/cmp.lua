c03053c4b930319c3440fac69ce4b10d88e6ffac
a-takamin <takamin0609@gmail.com>
2025-02-08 22:25:53 +0900

    lsp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -31,11 +31,11 @@ return {
 				completion = { completeopt = "menu,menuone,noinsert" },
 
 				mapping = cmp.mapping.preset.insert({
-					["<C-n>"] = cmp.mapping.select_next_item(),
-					["<C-p>"] = cmp.mapping.select_prev_item(),
+					["<C-j>"] = cmp.mapping.select_next_item(),
+					["<C-k>"] = cmp.mapping.select_prev_item(),
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-y>"] = cmp.mapping.confirm({ select = true }),
+					["<Space>"] = cmp.mapping.confirm({ select = true }),
 					["<C-Space>"] = cmp.mapping.complete({}),
 					["<C-l>"] = cmp.mapping(function()
 						if luasnip.expand_or_locally_jumpable() then
--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -6,6 +6,20 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup {}
+    local function my_on_attach(bufnr) 
+      local api = require "nvim-tree.api"
+      local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      end
+
+      -- デフォルトマッピング
+      api.config.mappings.default_on_attach(bufnr)
+
+      vim.keymap.set('n', '<leader>tt', api.tree.toggle, opts("Toggle Tree"))
+    end
+
+    require("nvim-tree").setup {
+      on_attach = my_on_attach,  -- 修正
+    }
   end,
 }
